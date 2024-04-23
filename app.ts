2297830f3e6cb5d8038a36ccf62e64d0fe44ad80
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-04-23 07:39:38 +0200

    se intenta agregar productos

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@ function displayProducts(products: Product[]): void {
     });
 }
 
-// Simulamos una lista de productos
+//lista de productos
 const products: Product[] = [
     { id: 1, name: 'Producto 1', price: 10 },
     { id: 2, name: 'Producto 2', price: 20 },
