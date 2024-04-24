c0a8c9739b49491083ff554bea9a0abe10d83f03
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-04-24 07:24:20 +0200

    cambio de precio en productos

--- app.ts
+++ app.ts
@@ -25,9 +25,9 @@ function displayProducts(products: Product[]): void {
 
 //lista de productos
 const products: Product[] = [
-    { id: 1, name: 'Producto 1', price: 10 },
-    { id: 2, name: 'Producto 2', price: 20 },
-    { id: 3, name: 'Producto 3', price: 30 }
+    { id: 1, name: 'Producto 1', price: 100 },
+    { id: 2, name: 'Producto 2', price: 200 },
+    { id: 3, name: 'Producto 3', price: 300 }
 ];
 
 // Llamamos a la función para mostrar los productos cuando la página se carga
