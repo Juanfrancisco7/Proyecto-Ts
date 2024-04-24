4975ab131369e4c8297a441f549b7b8a35a8bcdf
Juanfrancisco7 <juanbencomo25@gmail.com>
2024-04-24 07:35:12 +0200

    se compila codigo

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ function displayProducts(products) {
 }
 //lista de productos
 var products = [
-    { id: 1, name: 'Producto 1', price: 10 },
-    { id: 2, name: 'Producto 2', price: 20 },
-    { id: 3, name: 'Producto 3', price: 30 }
+    { id: 1, name: 'Producto 1', price: 100 },
+    { id: 2, name: 'Producto 2', price: 200 },
+    { id: 3, name: 'Producto 3', price: 300 }
 ];
 // Llamamos a la función para mostrar los productos cuando la página se carga
 window.onload = function () {
